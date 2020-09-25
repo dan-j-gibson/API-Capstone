8d2e241df96e26ccdc8eb95f1c20291cffa4765d
Dan Gibson <dgibson1124@gmail.com>
2020-09-25 09:20:02 -0700

    address conversion successful
    - function converting string to lon/lat coordinates successful and stored in variables

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
-'use strict'
+'use strict';
 // declares both API keys that will be used
 const fireApiKey ='0a9fe053b25841febdc85ad002225ad6'
 const geoApiKey ='b0f870fa197b0d'
 
 // builds base urls with API keys
-const geoUrl ='https://us1.locationiq.com/v1/search.php?key=' + geoApiKey;
+const geoUrl ='https://us1.locationiq.com/v1/search.php?key=' + geoApiKey + '&format=json&q=';
 const fireUrl ='https://api.breezometer.com/fires/v1/current-conditions?key=' + fireApiKey;
 
-// converts users location search to lat/lon format (geocoding)
-function convertAddress(addressInput) {}
 
+
+
+// }
+
+// converts users location search to lat/lon format (geocoding)
+function convertAddress(addressInput) {
+
+    const lonLatUrl = geoUrl + addressInput;
+// display new URL to fetch from API
+    console.log(lonLatUrl);
+    fetch(lonLatUrl)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw Error(`looks like there was an issue:`);
+    })
+    .then(responseJson => assignCoordinates(responseJson))
+    .catch(err => {
+      return alert(`${err.message}`);
+    });   
+}
+
+function assignCoordinates(responseJson) {
+    console.log(responseJson)
+    const lon = responseJson[0].lon;
+    const lat = responseJson[0].lat
+    // logs the first instance of lon and lat of location search
+       console.log('lat:' + lat,'lon:' + lon) 
+       
+    }
 
 // displays list of fire locations ordered by distance
-function displayResults(responseJson) {}
+// function displayResults(responseJson) {}
 
 // builds parameters for fire API
 function fireFormatQueryParams(params) {
@@ -29,30 +58,39 @@ function geoFormatQueryParams(params) {
   }
 
 // takes converted location input and radius and returns list of fires and their information
-function getFiresList(location,radius) {
+// function getFiresList(location,radius) {
+// console.log(location);
+// const params = {
+//     lon: jsonResponse.lon,
+//     lat: jsonRespone.lat
+// }
 
-
-    fetch()
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw Error(`looks like there was an issue: ${response.statusText}`);
-    })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-      return alert(`${err.message}`);
-    });   
-}
+//     fetch()
+//     .then(response => {
+//       if (response.ok) {
+//         displayResults(responseJson)    
+//         return response.json();
+//       }
+//       throw Error(`looks like there was an issue: ${response.statusText}`);
+//     })
+//     .then(responseJson => displayResults(responseJson)
+//     )
+//     .catch(err => {
+//       return alert(`${err.message}`);
+//     });   
+// }
 
 
 // pulls user's inputs and sends them to API functions
 function watchForm() {
-    $('form').submit(event => {
+    $('.location-form').submit(event => {
       event.preventDefault();
-      const searchLocation = convertAddress($('#js-search-location').val());
+      const searchLocation = $('#js-search-location').val();
       const searchRadius  = $('#js-search-radius').val();
-      getFiresList(searchLocation, searchRadius);
+      console.log(searchLocation);
+      console.log(searchRadius);
+      convertAddress(searchLocation);
+    //   getFiresList(searchLocation, searchRadius);
     });
   }
 
